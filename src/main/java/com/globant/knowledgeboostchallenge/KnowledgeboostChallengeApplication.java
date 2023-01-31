26bc876adc5a2c9e17bf916088236eb7cfcac2d6
Jose Luis Padilla Rodarte <joseluis.padilla@globant.com>
2023-01-31 04:19:32 -0600

    JLPR: creating REST client

--- src/main/java/com/globant/knowledgeboostchallenge/KnowledgeboostChallengeApplication.java
+++ src/main/java/com/globant/knowledgeboostchallenge/KnowledgeboostChallengeApplication.java
@@ -2,12 +2,18 @@ package com.globant.knowledgeboostchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class KnowledgeboostChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnowledgeboostChallengeApplication.class, args);
+	}
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/globant/knowledgeboostchallenge/controller/KnowledgeboostChallengeController.java
+++ src/main/java/com/globant/knowledgeboostchallenge/controller/KnowledgeboostChallengeController.java
@@ -1,17 +1,34 @@
 package com.globant.knowledgeboostchallenge.controller;
 
 import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/weather")
 public class KnowledgeboostChallengeController {
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/city/{cityName}")
     public String getByCity(@PathVariable String cityName) {
-        return "City " + cityName;
+        ResponseEntity<String> response
+                = restTemplate.getForEntity("http://localhost:8081/identity/message",
+                String.class);
+        return response.getBody();
+        /*HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <String> entity = new HttpEntity<String>(headers);
+
+        return restTemplate.exchange("
+                http://localhost:8080/products", HttpMethod.GET, entity, String.class).getBody();*/
+
+        //return "City " + cityName;
     }
     @GetMapping("/latitude/{latitude}/longitude/{longitude}")
     public String getByCoordinates(@PathVariable String latitude,
