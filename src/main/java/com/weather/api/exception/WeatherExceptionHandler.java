a191db54a8f34103ff31b7653820484739fdf129
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2023-02-01 11:02:57 -0600

    Adding Exception handler.

---
+++ src/main/java/com/weather/api/exception/WeatherExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.weather.api.exception;
+
+import com.weather.api.record.ResponseRecord;
+import com.weather.api.util.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class WeatherExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<ResponseRecord<String>> notFoundException(NotFoundException notFoundException) {
+        return new ResponseEntity<>(new ResponseRecord<>(ResponseMessage.FAILURE, notFoundException.getMessage(),
+                notFoundException.getCause(), null),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<ResponseRecord<String>> runtimeException(RuntimeException runtimeException) {
+        return new ResponseEntity<>(new ResponseRecord<>(ResponseMessage.FAILURE, runtimeException.getMessage(),
+                runtimeException.getCause(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
