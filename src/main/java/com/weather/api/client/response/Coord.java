f5f9b77636af96cb5ec5d9716ccbc99cf90f3f24
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2023-01-31 20:09:30 -0600

    Adding validations for Latitude and Longitude.

--- src/main/java/com/weather/api/client/response/Coord.java
+++ src/main/java/com/weather/api/client/response/Coord.java
@@ -4,6 +4,9 @@ package com.weather.api.client.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,8 +20,10 @@ import lombok.Setter;
 public class Coord {
 
     @JsonProperty("lon")
+    @DecimalMin("-180.0") @DecimalMax("180.0") @Digits(integer=3, fraction=6)
     private Double lon;
     @JsonProperty("lat")
+    @DecimalMin("-90.0") @DecimalMax("90.0") @Digits(integer = 2, fraction = 6)
     private Double lat;
 
 }
--- src/main/java/com/weather/api/controller/OpenWeatherController.java
+++ src/main/java/com/weather/api/controller/OpenWeatherController.java
@@ -1,13 +1,11 @@
 package com.weather.api.controller;
 
+import com.weather.api.client.response.Coord;
 import com.weather.api.client.response.WeatherResponse;
 import com.weather.api.record.ResponseRecord;
 import com.weather.api.service.WeatherService;
 import com.weather.api.util.ResponseMessage;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,13 +36,12 @@ public class OpenWeatherController {
                 weatherService.getWeatherByCityName(cityName)),  HttpStatus.OK);
     }
 
-    @GetMapping(value = "/latitude/{latitude}/longitude/{longitude}")
+    @GetMapping(value = "/latitude/{lat}/longitude/{lon}")
     public ResponseEntity<ResponseRecord<WeatherResponse>> getWeatherByLatitudeAndLongitude(
-            @PathVariable @DecimalMin("-90.0") @DecimalMax("90.0") @Digits(integer = 2, fraction = 6) Double latitude,
-            @PathVariable @DecimalMin("-180.0") @DecimalMax("180.0") @Digits(integer=3, fraction=6) Double longitude) {
-        log.info("Looking weather by latitude: {} and longitude: {}", latitude, longitude);
+            @Valid Coord coord) {
+        log.info("Looking weather by latitude: {} and longitude: {}", coord.getLat(), coord.getLon());
 
         return new ResponseEntity<>(new ResponseRecord<>(ResponseMessage.SUCCESS, LATITUDE_AND_LONGITUDE_OK, null,
-                weatherService.getWeatherByLatitudeAndLongitude(latitude, longitude)),  HttpStatus.OK);
+                weatherService.getWeatherByLatitudeAndLongitude(coord.getLat(), coord.getLon())),  HttpStatus.OK);
     }
 }
